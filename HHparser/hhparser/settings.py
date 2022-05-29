5bad81eba6a73e4922932d6d47cbc78c7e218d4f
Project_Orion <nosov.dmitry008@gmail.com>
2022-05-29 09:28:21 +0300

    Key

--- HHparser/hhparser/settings.py
+++ HHparser/hhparser/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from kluch import kluch
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-15z*9_up)pr9(+yd%qd)ei%jl%y@myxn+o0z*m%+6!)r*zccn5'
+SECRET_KEY = kluch
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
