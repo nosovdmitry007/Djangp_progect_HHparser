1bba18011e76278714758e998a7f4bc2b41abb3b
Project_Orion <nosov.dmitry008@gmail.com>
2022-07-12 15:45:48 +0300

    ADD AI Photo Filter

--- HHparser/api_request.py
+++ HHparser/api_request.py
@@ -1,8 +1,6 @@
 import requests
 import pprint
 from .hhparser.kluch import my_kluch
-# response = requests.get('http://127.0.0.1:8000/api/skills/', auth=('Dima', '1234'))
-
 
 headers = {'Authorization': f'Token {my_kluch()}'}
 response = requests.get('http://127.0.0.1:8000/api/skills/',  headers=headers)
