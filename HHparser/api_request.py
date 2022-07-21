3cca9969a1341e40be92ffebc1a88e8503d74430
nosovdmitry007 <88159866+nosovdmitry007@users.noreply.github.com>
2022-07-21 16:16:11 +0300

    api correct

--- HHparser/api_request.py
+++ HHparser/api_request.py
@@ -1,7 +1,6 @@
 import requests
 import pprint
 from .hhparser.kluch import token_my
-# response = requests.get('http://127.0.0.1:8000/api/skills/', auth=('Dima', '1234'))
 
 token = token_my()
 headers = {'Authorization': f'Token {token}'}
