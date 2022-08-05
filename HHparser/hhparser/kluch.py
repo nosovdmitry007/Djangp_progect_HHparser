63deb834fb45ed5af48c9068c6ca294952b22b26
nosovdmitry007 <88159866+nosovdmitry007@users.noreply.github.com>
2022-08-05 20:51:37 +0300

    Delete kluch.py

--- HHparser/hhparser/kluch.py
+++
@@ -1,7 +0,0 @@
-def kluch():
-    z='django-insecure-15z*9_up)pr9(+yd%qd)ei%jl%y@myxn+o0z*m%+6!)r*zccn5'
-    return z
-
-def my_kluch():
-    j='0d28a6c0e74b7cf4cdace964b92866353371d3b9'
-    return j
